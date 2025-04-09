4d002069fd30826297dd2fa13ffce5a9d6461551
Abhi_6008 <163118270+GonelaAbhi@users.noreply.github.com>
2025-04-09 23:02:14 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -2,5 +2,7 @@ public class abc{
 	public static void main(String Args[]){
 		System.out.println("hello");
 			System.out.println("hellouhfhf");
+			System.out.println("hello");
+			System.out.println("hellouhfhf");
 		}
 	}
